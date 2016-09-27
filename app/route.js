0247c1364e6750afa607bbcf0ef8998f332852d4
Conor Cussell <contact@conorcussell.co.uk>
2016-09-27 20:27:34 +0100

    get basic version working

--- app/route.js
+++ app/route.js
@@ -1,21 +1,77 @@
-var ipc = require('electron').ipcRenderer
-var searchInput = document.querySelector('.js-search');
-var $ = require('jquery');
+const ipc = require('electron').ipcRenderer;
+const $ = require('jquery');
+const open = require("open");
 
-searchInput.addEventListener('input', search);
+const citymapperApiKey = require('../secrets').key;
 
-ipc.on('show', function (event, message) {
-  searchInput.focus()
+const fromInput = document.querySelector('.js-from-input');
+fromInput.addEventListener('input', updateFrom);
+
+const toInput = document.querySelector('.js-to-input');
+toInput.addEventListener('input', updateTo);
+
+ipc.on('show', () => {
+  fromInput.focus()
 });
 
 const googleurl = 'http://maps.googleapis.com/maps/api/geocode/json?address=';
 
-function search(e) {
-  $.getJSON(googleurl + e.currentTarget.value, (response) => {
-    if (response && response.results) {
-      document.querySelector('.js-results').textContent = response.results[0].geometry.location.lat + ' ' + response.results[0].geometry.location.lng;
+let fromPos = {};
+let toPos = {};
+
+function getCoordsFromPostCode(postcode, position) {
+  $.getJSON(googleurl + postcode, (response) => {
+    if (response.results) {
+      position.lat = response.results[0].geometry.location.lat;
+      position.lng = response.results[0].geometry.location.lng;
     }
   });
 }
 
-const mapurl = 'https://developer.citymapper.com/api/1/traveltime/?startcoord=51.525246%2C0.084672&endcoord=51.559098%2C0.074503&time=2014-11-06T19%3A00%3A02-0500&time_type=arrival&key=0365e072d0b665923245fa37d281f2f8'
+function updateFrom(e) {
+  getCoordsFromPostCode(e.currentTarget.value, fromPos);
+}
+
+function updateTo(e) {
+  getCoordsFromPostCode(e.currentTarget.value, toPos);
+}
+
+const getTime = document.querySelector('.js-go');
+getTime.addEventListener('click', getTravelTime);
+
+function routeApiUrl(startcoord, endcoord) {
+  console.log(citymapperApiKey);
+  return `https://developer.citymapper.com/api/1/traveltime/?startcoord=${startcoord}&endcoord=${endcoord}&key=${citymapperApiKey}`;
+}
+
+function makeLocation(pos) {
+  return `${pos.lat},${pos.lng}`;
+}
+
+function getTravelTime() {
+  const start = makeLocation(fromPos);
+  const end = makeLocation(toPos);
+  $.getJSON(routeApiUrl(start, end), (response) => {
+    if (response.travel_time_minutes) {
+      document.querySelector('.js-mins').textContent = `${response.travel_time_minutes} minutes by 🚌`;
+      addUrlToLink(start, end);
+    }
+  });
+}
+
+function cityMapperUrl(fromLocation, toLocation) {
+  return `https://citymapper.com/directions?startcoord=${fromLocation}&endcoord=${toLocation}`;
+}
+
+const citymapperLink = document.querySelector('.js-city-mapper');
+citymapperLink.addEventListener('click', visitCitymapper);
+
+function addUrlToLink(start, end) {
+  citymapperLink.setAttribute('href', cityMapperUrl(start, end));
+  citymapperLink.style.display = 'block';
+}
+
+function visitCitymapper(e) {
+  e.preventDefault();
+  open(this.getAttribute('href'));
+}
